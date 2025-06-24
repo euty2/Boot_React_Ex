e293f7b03cc472cb7c3284f130847bb936ffd817
SJF05 <lakors05@gmail.com>
2025-06-24 12:04:07 +0900

    Car insert test

--- cardatabase/src/main/java/org/suhodo/cardatabase/domain/Car.java
+++ cardatabase/src/main/java/org/suhodo/cardatabase/domain/Car.java
@@ -5,14 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Getter
-@Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
--- cardatabase/src/test/java/org/suhodo/cardatabase/CardatabaseApplicationTests.java
+++ cardatabase/src/test/java/org/suhodo/cardatabase/CardatabaseApplicationTests.java
@@ -1,13 +1,50 @@
 package org.suhodo.cardatabase;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.suhodo.cardatabase.domain.Car;
+import org.suhodo.cardatabase.repository.CarRepository;
 
 @SpringBootTest
 class CardatabaseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private CarRepository carRepository;
 
+	@Test
+	public void TestCarRepository(){
+		Car fordCar = Car.builder()
+						.brand("Ford")
+						.model("Mustang")
+						.color("Red")
+						.registrationNumber("T-1111")
+						.modelYear(2025)
+						.price(5400)
+						.build();
+		
+		carRepository.save(fordCar);
+
+		Car kiaCar = Car.builder()
+						.brand("Kia")
+						.model("K-5")
+						.color("Silver")
+						.registrationNumber("K-111")
+						.modelYear(2025)
+						.price(6400)
+						.build();
+		
+		carRepository.save(kiaCar);
+
+		Car hyundaiCar = Car.builder()
+						.brand("Hyundai")
+						.model("Genesis")
+						.color("Black")
+						.registrationNumber("H-111")
+						.modelYear(2025)
+						.price(7400)
+						.build();
+		
+		carRepository.save(hyundaiCar);
+	}
 }
