311c1f6f981fd99bf38239f4b3252f3839dad96b
SJF05 <lakors05@gmail.com>
2025-06-24 15:43:52 +0900

    Swagger settings

--- cardatabase/src/main/java/org/suhodo/cardatabase/controller/CarController.java
+++ cardatabase/src/main/java/org/suhodo/cardatabase/controller/CarController.java
@@ -9,6 +9,12 @@ import org.suhodo.cardatabase.repository.CarRepository;
 
 import lombok.RequiredArgsConstructor;
 
+/*
+ * Client : (브라우저상의) React App: Axios/Fetch 라이브러리 사용
+ * Server : (톰캣을 연동한) Spring Boot App: @RestController 설정 클래스
+ * 통신 : application/json 헤더에 설정
+ * 데이터 : json 문자열(js객체를 문자열로 변환)
+ */
 @RestController
 @RequiredArgsConstructor
 public class CarController {
@@ -20,3 +26,21 @@ public class CarController {
         return carRepository.findAll();
     }
 }
+
+/*
+ * [Rest Controller 생성하는 법]
+ * 1. Class위에 @RestController을 붙인다.
+ *    그리고 원하는 주소에 매핑된 메서드를 생성한다.
+ * 
+ * 2. implementation 'org.springframework.boot:spring-boot-starter-data-rest'
+ *    를 사용해서, 기존의 repository를 repository/controller의 2가지 용도로
+ *    즉시 사용하게 하는 방법이다.
+ *    spring.data.rest.basePath=/api
+ *    이 주소가 접근 경로이다.
+ * 
+ *    GET             : 읽기(Read)
+ *    POST            : 생성(Create)
+ *    PUT             : 업데이트(Update), 모든 필드를 전송
+ *    PATCH           : 업데이트(Update), 업데이트 할 필드만 전송
+ *    DELETE          : 삭제(Delete)
+ */
--- cardatabase/src/main/java/org/suhodo/cardatabase/repository/CarRepository.java
+++ cardatabase/src/main/java/org/suhodo/cardatabase/repository/CarRepository.java
@@ -1,8 +1,19 @@
 package org.suhodo.cardatabase.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.suhodo.cardatabase.domain.Car;
 
+
+@RepositoryRestResource
 public interface CarRepository extends JpaRepository<Car, Long> {
 
+    // 브랜드로 자동차를 검색하는 Query Method/Rest 엔드포인트 등록
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    // 색상으로 자동차를 검색하는 Query Method/Rest 엔드포인트 등록
+    List<Car> findByColor(@Param("color") String color);
 }
